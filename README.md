b6ed8a291c6a82fc1b1e6d40ec3a084cb6ae93ed
Maurits <maurits@mauritskaptein.com>
2020-02-21 14:17:27 +0100

    Added first structure of package.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # sclblpy
+> Last edited 21-02-2020; McK.
+
 Python package for Scailable uploads
 
 Functionally this package allows one to upload fitted sci-kit learn models to Scailabe after authentication using JWT:
@@ -17,3 +19,6 @@ sp.upload(mod)
 
 * We use [http://google.github.io/styleguide/pyguide.html](http://google.github.io/styleguide/pyguide.html).
 * Package structure: [https://packaging.python.org/tutorials/packaging-projects/](https://packaging.python.org/tutorials/packaging-projects/)
+
+## Notes:
+Install package locally ``pip install -e .``
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sclblpy",
+    version="0.0.1",
+    author="Maurits Kaptein",
+    author_email="maurits.kaptein@scailable.com",
+    description="Python package for uploading models to Scailable toolchain.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/scailable/sclblpy/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
