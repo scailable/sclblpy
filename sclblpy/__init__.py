43b18e4498221a6b85ce38abf864033075f82acc
Maurits <maurits@mauritskaptein.com>
2020-02-27 08:10:23 +0100

    Minor change catching forgotten import for exposing in package. Now no more "todo's" other than the connection to the toolchain.

--- sclblpy/__init__.py
+++ sclblpy/__init__.py
@@ -1,7 +1,5 @@
 """
 The sclblpy package allows users to upload, monitor, and administer models and REST endpoints directly from python.
 """
-# Todo(McK) add the other exposed functions.
-
-from .main import upload, remove_credentials
+from .main import upload, endpoints, delete_endpoint, remove_credentials
 
